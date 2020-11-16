b3f097ce4841cd0ece58a6e92ee3ea560df141e1
Trofimov Ivan <allvisionk@mail.ru>
2020-11-16 13:18:19 +0300

    First commit. Only one type of cells

---
+++ cells_classes_module.py
@@ -0,0 +1,11 @@
+WIDTH = 400
+HEIGHT = 600
+
+
+class FirstCell():
+    def __init__(self):
+        self.multiply = 0.5
+        self.center = (WIDTH//2, HEIGHT//2)
+        self.r = 10
+        self.time_multiply = 2
+
