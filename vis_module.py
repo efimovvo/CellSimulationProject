b63d237ffec748c695d2fe4c014573b28687bb3f
Trofimov Ivan <allvisionk@mail.ru>
2020-11-17 04:12:35 +0300

    added documentation

--- vis_module.py
+++ vis_module.py
@@ -1,5 +1,5 @@
 import pygame
-WIDTH = 400
+WIDTH = 600
 HEIGHT = 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -9,6 +9,12 @@ BLUE = (0, 0, 255)
 
 
 def draw_cells(list_cells, surf):
+    """
+    :arg    list_cells - list of cells, list
+            surf - surface where cells will be drawn
+
+    Function draws a cells on the surface."""
+    surf.fill(WHITE)
     for cell in list_cells:
         pygame.draw.circle(surf, RED, cell.center, cell.r)
         pygame.draw.circle(surf, BLACK, cell.center, cell.r, 1)
