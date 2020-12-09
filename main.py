1ae6e1270afccf46e993a7359d02ba6d75e2d47f
Ivan Trofimov <allvisionk@mail.ru>
2020-12-09 20:36:49 +0300

    Переместил рисование графиков, добавление кнопок в отдельные функции, добавил функцию изменение параметров клеток : изменение шага старения и способности к размножению

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def add_predator(position, list_cell):
         new_cell.predator = True
         new_cell.color = RED
         new_cell.position = np.array(position)
-        new_cell.engines = 2 + (random.random() * 2)**0.5
+        new_cell.engines = 2 + (random.random() * 2) ** 0.5
         new_cell.satiety_step = 0.005
         new_cell.reproductive_age = [5, 50]
         new_cell.reproductive_waiting = 3
@@ -73,21 +73,135 @@ def find_button(positon, button_list):
                 button_list - type : list, list of different buttons
     Function activates the button if it is clicked"""
     for button in button_list:
-        if (button.position[0] <= positon[0] <= button.position[0] + button.size[0]
-                and button.position[1] <= positon[1] <= button.position[1] + button.size[1]):
-            button.click()
+        if not button.switch:
+            if (button.position[0] <= positon[0] <= button.position[0] + button.size[0]
+                    and button.position[1] <= positon[1] <= button.position[1] + button.size[1]):
+                button.click()
+        else:
+            if (button.position[0] <= positon[0] <= button.position[0] + button.size[0]
+                    and button.position[1] <= positon[1] <= button.position[1] + button.size[1]):
+                button.status = 1
 
 
-def main():
-    """Main function of program. It creates a screen where cells lives and makes an actions with them"""
+def buttons():
+    """Function returns list of buttons"""
+
     offset = 10
-    button_graph = Button([offset, offset], [50, PANEL_HEIGHT - 2 * offset], 'Button')
+    # change graph button :
+    button_graph = Button([offset, offset], [100, PANEL_HEIGHT - 2 * offset], 'Change graph')
+
     button_decrease = Button([button_graph.position[0] + button_graph.size[0] + 10, button_graph.position[1]],
                              [0.5 * button_graph.size[0], 0.5 * button_graph.size[1]], '-')
     button_increase = Button([button_graph.position[0] + 2 * button_graph.size[0] + 20 + button_decrease.size[0],
                               button_graph.position[1]], [0.5 * button_graph.size[0], 0.5 * button_graph.size[1]], '+')
 
-    button_list = [button_graph, button_decrease, button_increase]  # creates a button list
+    # change age step button :
+    button_change_age_step = Button([400, 10], [100, 20], 'Change age step')
+    button_age_step_increase = Button([510, 10], [20, 20], '+')
+    button_age_step_decrease = Button([370, 10], [20, 20], '-')
+
+    # change multiply skill button
+    button_change_multiply_skill = Button([600, 10], [130, 20], 'Change multiply skill')
+    button_multiply_skill_increase = Button([750, 10], [20, 20], '+')
+    button_multiply_skill_decrease = Button([570, 10], [20, 20], '-')
+
+    # indicate what type of buttons
+    button_increase.switch = button_decrease.switch = button_age_step_increase.switch = \
+        button_age_step_decrease.switch = button_multiply_skill_increase.switch = \
+        button_multiply_skill_decrease.switch = True
+
+
+    button_list = [button_graph, button_decrease, button_increase,
+                   button_change_age_step, button_age_step_increase, button_age_step_decrease,
+                   button_change_multiply_skill, button_multiply_skill_decrease, button_multiply_skill_increase]
+
+    return button_list
+
+
+def graphs(surf, button_list, time_list, victims_list, predators_list,
+           victims_list_mid_age, predators_list_mid_age, victims_list_mid_engine, predators_list_mid_engine,
+           victims_list_mid_satiety, predators_list_mid_satiety):
+
+    """Function draws graphs on the surf. It depends what type of button is pushed"""
+
+    # for 0 type :
+    if button_list[0].status == 0:
+        draw_graph(surf,
+                   starting_point=[SCREEN_WIDTH, PANEL_HEIGHT],
+                   sizes=[PLOT_AREA_WIDTH, SCREEN_HEIGHT // 2],
+                   x_data=time_list,
+                   y_data=[victims_list, predators_list],
+                   axis_comment=["Время, шаг симуляции", "Популяция, шт."],
+                   graph_name="График зависимости размера популяции от времени")
+
+    # for 1 type :
+    elif button_list[0].status == 1:
+        draw_graph(surf,
+                   starting_point=[SCREEN_WIDTH, PANEL_HEIGHT],
+                   sizes=[PLOT_AREA_WIDTH, SCREEN_HEIGHT // 2],
+                   x_data=time_list,
+                   y_data=[victims_list_mid_age, predators_list_mid_age],
+                   axis_comment=["Время, шаг симуляции", "Средний возраст популяции, ед. возраста"],
+                   graph_name="График зависимости возраста популяции от времени")
+
+    # for 2 type :
+    elif button_list[0].status == 2:
+        draw_graph(surf,
+                   starting_point=[SCREEN_WIDTH, PANEL_HEIGHT],
+                   sizes=[PLOT_AREA_WIDTH, SCREEN_HEIGHT // 2],
+                   x_data=time_list,
+                   y_data=[victims_list_mid_engine, predators_list_mid_engine],
+                   axis_comment=["Время, шаг симуляции", "Средняя подвижность по популяции, ед. подвижности"],
+                   graph_name="График зависимости возраста популяции от времени")
+    # for 3 type
+    else:
+        draw_graph(surf,
+                   starting_point=[SCREEN_WIDTH, PANEL_HEIGHT],
+                   sizes=[PLOT_AREA_WIDTH, SCREEN_HEIGHT // 2],
+                   x_data=time_list,
+                   y_data=[victims_list_mid_satiety, predators_list_mid_satiety],
+                   axis_comment=["Время, шаг симуляции", "Средняя сытость по популяции, % сытости"],
+                   graph_name="График зависимости возраста популяции от времени")
+
+
+def cells_parameters(list_cell, button_list):
+    """:arg :   list_cell - type : list, each element is Cell type
+                button_list - type : list, each element if Button type
+
+    Function changes cells parameters if button was pressed.
+    """
+    # decrease max food number :
+    if button_list[1].status == 1:
+        change_food_max_quantity(2)
+        button_list[1].status = 0
+
+    # increase max food number :
+    elif button_list[2].status == 1:
+        change_food_max_quantity(1)
+        button_list[2].status = 0
+
+    # increase age step :
+    elif button_list[4].status == 1:
+        change_age_step(list_cell, 1)
+        button_list[4].status = 0
+    # decrease age step :
+    elif button_list[5].status == 1:
+        change_age_step(list_cell, 2)
+        button_list[5].status = 0
+
+    # decrease multiply skill :
+    elif button_list[7].status == 1:
+        change_multiply_skill(list_cell, 2)
+        button_list[7].status = 0
+    # increase multiply skill :
+    elif button_list[8].status == 1:
+        change_multiply_skill(list_cell, 1)
+        button_list[8].status = 0
+
+
+def main():
+    """Main function of program. It creates a screen where cells lives and makes an actions with them"""
+    button_list = buttons()  # creates a button list
     cell_list, meal_list = restart_the_game()  # creates the cells and the meal lists
     time = 0
     pygame.init()
@@ -129,7 +243,7 @@ def main():
         draw_cells(cell_list, screen)
 
         # Draw interface objects
-        draw_user_panel(screen, button_list)
+        draw_user_panel(screen, button_list, cell_list[0])
         # Draw population data
         (time_list,
          victims_list, predators_list,
@@ -137,38 +251,14 @@ def main():
          victims_list_mid_engine, predators_list_mid_engine,
          victims_list_mid_satiety, predators_list_mid_satiety) = read_data()
         if len(time_list) > 0:
-            if button_list[0].status == 0:
-                draw_graph(screen,
-                           starting_point=[SCREEN_WIDTH, PANEL_HEIGHT],
-                           sizes=[PLOT_AREA_WIDTH, SCREEN_HEIGHT // 2],
-                           x_data=time_list,
-                           y_data=[victims_list, predators_list],
-                           axis_comment=["Время, шаг симуляции", "Популяция, шт."],
-                           graph_name="График зависимости размера популяции от времени")
-            elif button_list[0].status == 1:
-                draw_graph(screen,
-                           starting_point=[SCREEN_WIDTH, PANEL_HEIGHT],
-                           sizes=[PLOT_AREA_WIDTH, SCREEN_HEIGHT // 2],
-                           x_data=time_list,
-                           y_data=[victims_list_mid_age, predators_list_mid_age],
-                           axis_comment=["Время, шаг симуляции", "Средний возраст популяции, ед. возраста"],
-                           graph_name="График зависимости возраста популяции от времени")
-            elif button_list[0].status == 2:
-                draw_graph(screen,
-                           starting_point=[SCREEN_WIDTH, PANEL_HEIGHT],
-                           sizes=[PLOT_AREA_WIDTH, SCREEN_HEIGHT // 2],
-                           x_data=time_list,
-                           y_data=[victims_list_mid_engine, predators_list_mid_engine],
-                           axis_comment=["Время, шаг симуляции", "Средняя подвижность по популяции, ед. подвижности"],
-                           graph_name="График зависимости возраста популяции от времени")
-            else:
-                draw_graph(screen,
-                           starting_point=[SCREEN_WIDTH, PANEL_HEIGHT],
-                           sizes=[PLOT_AREA_WIDTH, SCREEN_HEIGHT // 2],
-                           x_data=time_list,
-                           y_data=[victims_list_mid_satiety, predators_list_mid_satiety],
-                           axis_comment=["Время, шаг симуляции", "Средняя сытость по популяции, % сытости"],
-                           graph_name="График зависимости возраста популяции от времени")
+            graphs(screen, button_list, time_list,
+                   victims_list, predators_list,
+                   victims_list_mid_age, predators_list_mid_age,
+                   victims_list_mid_engine, predators_list_mid_engine,
+                   victims_list_mid_satiety, predators_list_mid_satiety)
+
+            cells_parameters(cell_list, button_list)
+
         if len(victims_list) > 0:
             draw_graph(screen,
                        starting_point=[SCREEN_WIDTH, PANEL_HEIGHT + SCREEN_HEIGHT // 2],
